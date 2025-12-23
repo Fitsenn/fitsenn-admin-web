714077280dcb1b674d1526fb92f2999e6525fae9
cristinaivanov <ivanov.cristina99@gmail.com>
2025-12-23 14:56:40 +0200

    chore: form changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 
 import { createRoot } from 'react-dom/client';
 
-import App from './app-root';
+import App from './app/app-root';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
